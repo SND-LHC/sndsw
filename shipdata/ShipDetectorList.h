e5ff0cbec7602627a3f001bad233604de681df1e
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-12-03 19:42:08 +0100

    removing conflict label

--- shipdata/ShipDetectorList.h
+++ shipdata/ShipDetectorList.h
@@ -12,5 +12,4 @@
  enum DetectorId {kEmulsionDet,kLHCScifi,kMuFilter, kVETO, kTimeDet, ktauRpc, ktauHpt, ktauTT, ktauTarget, kStraw, kecal, khcal, kMuon , kPreshower, kTRSTATION, kSplitCal, kBox1, kSpectrometer\
 			  , kPixelModules, kSciFi, kScintillator, kMufluxSpectrometer, kMuonTagger, kUpstreamTagger, kEndOfList};
 // last one for UpstreamTagger, then last five for muonflux and Charm measurement, first three for ship@lhc
->>>>>>> abuonaura/SHiP@LHC-Tungsten
 #endif
