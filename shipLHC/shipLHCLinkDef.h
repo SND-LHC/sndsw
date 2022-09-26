039f493ccc6f973caf94e6ea62fd2502a89e1a4b
dcentanni <daniele.centanni@outlook.it>
2022-09-26 17:30:43 +0200

    Implementation of AdvMuFilter + CB removal

--- shipLHC/shipLHCLinkDef.h
+++ shipLHC/shipLHCLinkDef.h
@@ -20,6 +20,8 @@
 #pragma link C++ class MagnetPoint+;
 #pragma link C++ class AdvTarget+;
 #pragma link C++ class AdvTargetPoint+;
+#pragma link C++ class AdvMuFilter+;
+#pragma link C++ class AdvMuFilterPoint+;
 #pragma link C++ class SNDLHCEventHeader;
 #pragma link C++ class sndRecoTrack+;
 
