34dbfa464dbe6a9679e6926f573aecbb4aaa4441
Oliver Lantwin <oliver.lantwin@cern.ch>
2023-08-25 17:41:51 +0200

    Improve clustering and add links to target clusters

--- python/SndlhcDigi.py
+++ python/SndlhcDigi.py
@@ -29,6 +29,9 @@ class SndlhcDigi:
         self.digiTarget2MCPointsBranch = self.sTree.Branch("Digi_TargetHits2MCPoints",self.digiTarget2MCPoints, 32000, 1)
         self.digiTargetCluster = ROOT.TClonesArray("AdvTargetHit")
         self.digiTargetClusterBranch = self.sTree.Branch("Digi_advTargetClusters", self.digiTargetCluster, 32000, 1)
+        self.digiTargetCluster2MCPoints = ROOT.TClonesArray("Hit2MCPoints")
+        self.digiTargetCluster2MCPoints.BypassStreamer(ROOT.kTRUE)
+        self.digiTargetCluster2MCPointsBranch = self.sTree.Branch("Digi_TargetClusterHits2MCPoints",self.digiTargetCluster2MCPoints, 32000, 1)
 
         lsOfGlobals  = ROOT.gROOT.GetListOfGlobals()
         self.scifiDet = lsOfGlobals.FindObject('Scifi')
@@ -87,8 +90,10 @@ class SndlhcDigi:
         self.digiTargetBranch.Fill()
         self.digiTarget2MCPointsBranch.Fill()
         self.digiTargetCluster.Delete()
+        self.digiTargetCluster2MCPoints.Delete()
         self.clusterTarget()
         self.digiTargetClusterBranch.Fill()
+        self.digiTargetCluster2MCPointsBranch.Fill()
 
     def digitizeScifi(self):
         hitContainer = {}
@@ -178,10 +183,10 @@ class SndlhcDigi:
             allPoints = ROOT.std.vector('AdvTargetPoint*')()
             for p in hitContainer[detID]:
                  allPoints.push_back(p)
-            aHit = ROOT.AdvTargetHit(detID, allPoints)
 
-            if self.digiTarget.GetSize() == index: self.digiTarget.Expand(index + 100)
-            self.digiTarget[index] = aHit
+            if self.digiTarget.GetSize() == index:
+                self.digiTarget.Expand(index + 100)
+            self.digiTarget[index] = ROOT.AdvTargetHit(detID, allPoints)
             for k in mcPoints[detID]:
                 mcLinks.Add(detID, k, mcPoints[detID][k] / norm[detID])
         self.digiTarget2MCPoints[0] = mcLinks
@@ -221,8 +226,9 @@ class SndlhcDigi:
                    cprev = c
 
     def clusterTarget(self):
-        index = 0
+        cluster_index = 0
         hitDict = {}
+        mcLinks = ROOT.Hit2MCPoints()
         for k, p in enumerate(self.sTree.Digi_advTargetHits):
             if not p.isValid():
                 continue
@@ -242,23 +248,39 @@ class SndlhcDigi:
                    if (c-cprev) == 1:
                         tmp.append(c)
                    if (c-cprev) != 1 or c == hitList[last]:
-                        first = tmp[0]
-                        N = len(tmp)
-                        hits.clear()
-                        for hit in tmp:
-                            hits.push_back(self.sTree.Digi_advTargetHits[hitDict[hit]],)
-                        # Take leading hit of each cluster, discard all other
-                        signals = ROOT.RVec("double")([h.GetSignal() for h in hits])
-                        leading_hit = hits[ROOT.VecOps.ArgMax(signals)]
-                        if self.digiTargetCluster.GetSize() == index:
-                            self.digiTargetCluster.Expand(index + 10)
-                        # TODO Improvement: sum energies and copy links from discarded hits
-                        self.digiTargetCluster[index] = leading_hit
-                        index += 1
-                        if c != hitList[last]:
-                             ncl += 1
-                             tmp = [c]
+                       mcPoints = {}
+                       norm = 0
+                       first = tmp[0]
+                       N = len(tmp)
+                       hits.clear()
+                       for hit in tmp:
+                           hits.push_back(self.sTree.Digi_advTargetHits[hitDict[hit]],)
+                       # Take leading hit of each cluster, discard all other
+                       signals = ROOT.RVec("double")([h.GetSignal() for h in hits])
+                       leading_hit = hits[ROOT.VecOps.ArgMax(signals)]
+                       detID = leading_hit.GetDetectorID()
+                       allPoints = ROOT.std.vector('AdvTargetPoint*')()
+                       # Use truth to find all points (shortcut, can be done using hits directly)
+                       link = self.sTree.Digi_TargetHits2MCPoints[0]
+                       for hit in hits:
+                           wlist = link.wList(hit.GetDetectorID())
+                           point_indices = [index for index,_ in wlist]
+                           for index, _ in wlist:
+                               point = self.sTree.AdvTargetPoint[index]
+                               allPoints.push_back(point)
+                               mcPoints[index] = point.GetEnergyLoss()
+                               norm += point.GetEnergyLoss()
+                       for index in mcPoints:
+                           mcLinks.Add(detID, index, mcPoints[index] / norm)
+                       if self.digiTargetCluster.GetSize() == cluster_index:
+                           self.digiTargetCluster.Expand(cluster_index + 10)
+                       self.digiTargetCluster[cluster_index] = ROOT.AdvTargetHit(detID, allPoints)
+                       cluster_index += 1
+                       if c != hitList[last]:
+                            ncl += 1
+                            tmp = [c]
                    cprev = c
+        self.digiTargetCluster2MCPoints[0] = mcLinks
             
     def finish(self):
         print('finished writing tree')
