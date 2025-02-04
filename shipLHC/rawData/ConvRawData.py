931f7b6f7b4921190f830a3e70e398371d7da3e9
siilieva <simona.ilieva.ilieva@cern.ch>
2025-02-04 19:04:30 +0100

    Set log severity to error to suppress annoying warning
    which is shown per event! since the conversion task doesn't set the FairFileSource.

--- shipLHC/rawData/ConvRawData.py
+++ shipLHC/rawData/ConvRawData.py
@@ -118,7 +118,7 @@ class ConvRawDataPY(ROOT.FairTask):
       logger = ROOT.FairLogger.GetLogger()
       logger.SetColoredLog(True)
       logger.SetLogVerbosityLevel('low')
-      logger.SetLogScreenLevel('warn')
+      logger.SetLogScreenLevel('error')
       logger.SetLogToScreen(True)
       if options.debug:
          logger.SetLogToFile(True)
