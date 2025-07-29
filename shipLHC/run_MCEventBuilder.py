90fa2cc9b4f6f4b9fd6f8c20d035007d00e0b3ad
Pedro1102 <134416660+Pedro1102@users.noreply.github.com>
2025-07-29 15:14:32 +0200

    Corrected grammar mistake.
    Co-authored-by: Oliver Lantwin <oliver.lantwin@cern.ch>

--- shipLHC/run_MCEventBuilder.py
+++ shipLHC/run_MCEventBuilder.py
@@ -13,7 +13,7 @@ parser.add_argument("-g", "--geoFile", help="geo file")
 parser.add_argument("-o", "--outputFile", help="Output file")
 parser.add_argument("--firstEvent", type=int, default=0, help="First event to process")
 parser.add_argument("--nEvents", type=int, default=0, help="Number of events to process (0 = all)")
-parser.add_argument("--saveOnlyFirst25", action="store_true", help="Only store the firs 25ns chunk of each event")
+parser.add_argument("--saveOnlyFirst25", action="store_true", help="Only store the first 25 ns chunk of each event")
 options = parser.parse_args()
 
 # ------------ Geo setup ----------------
