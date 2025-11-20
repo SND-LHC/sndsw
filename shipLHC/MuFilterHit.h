e65246c08d51e3c4fb25ee3818f41ddba6870611
siilieva <simona.ilieva.ilieva@cern.ch>
2025-11-20 17:33:46 +0100

    MuFilterHit: Add option to skip small SiPMs in numerous functions
    Skipping small SiPMs is the default behaviour since these channels have delayed and polluted response,
    see #291 and #296.

--- shipLHC/MuFilterHit.h
+++ shipLHC/MuFilterHit.h
@@ -27,15 +27,15 @@ class MuFilterHit : public SndlhcHit
 
     /** Output to screen **/
     void Print() const;
-    Float_t GetEnergy();
+    Float_t GetEnergy(Bool_t use_short=kFALSE);
     Float_t SumOfSignals(char* opt,Bool_t mask=kTRUE);
     std::map<TString,Float_t> SumOfSignals(Bool_t mask=kTRUE);
-    std::map<Int_t,Float_t> GetAllSignals(Bool_t mask=kTRUE,Bool_t positive=kTRUE);
-    std::map<Int_t,Float_t> GetAllTimes(Bool_t mask=kTRUE,Bool_t positive=kTRUE);
-    Float_t  GetDeltaT(Bool_t mask=kTRUE,Bool_t positive=kTRUE);
-    Float_t  GetFastDeltaT(Bool_t mask=kTRUE,Bool_t positive=kTRUE);
-    Float_t  GetImpactT(Bool_t mask=kTRUE,Bool_t positive=kTRUE);
-    Float_t  GetImpactXpos(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t isMC=kFALSE);
+    std::map<Int_t,Float_t> GetAllSignals(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE);
+    std::map<Int_t,Float_t> GetAllTimes(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE);
+    Float_t  GetDeltaT(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE);
+    Float_t  GetFastDeltaT(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE);
+    Float_t  GetImpactT(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE);
+    Float_t  GetImpactXpos(Bool_t mask=kTRUE,Bool_t positive=kTRUE,Bool_t use_short=kFALSE,Bool_t isMC=kFALSE);
     bool isValid() const {return flag;}
     bool isMasked(Int_t i) const {return fMasked[i];}
     void SetMasked(Int_t i) {fMasked[i]=kTRUE;}
