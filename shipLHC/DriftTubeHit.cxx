755c743a79859de3bcf7a8fdd1dc5377a62e77f0
siilieva <simona.ilieva.ilieva@cern.ch>
2026-09-07 14:26:08 +0200

    geo: Add drift tube's spatial alignment: version 1
    The GetPosition methods of both the detector and the hit have to
    be adapted. To get the laterality one uses det id and not positions
    so the dt conversion doesnt need to be redone post alignment.

--- shipLHC/DriftTubeHit.cxx
+++ shipLHC/DriftTubeHit.cxx
@@ -69,11 +69,31 @@ void DriftTubeHit::Print()
 
 TVector3 DriftTubeHit::GetPosition() { 
    TVector3 position {};
-   std::string node = Form("/Detector_0/volDriftTubePlane_%d/volLayer_%d/volCell_%d/volAnode_2", GetPlane(), GetLayer(), GetDetectorID());
+   int plane = GetPlane();
+   int layer = GetLayer();
+   std::string layer_str = std::to_string(layer);
+   std::string node = Form("/Detector_0/volDriftTubePlane_%d/volLayer_%d/volCell_%d/volAnode_2", plane, layer, GetDetectorID());
    auto navigator = gGeoManager->GetCurrentNavigator();
 
    DriftTube *DriftTubeDet = dynamic_cast<DriftTube *>(gROOT->GetListOfGlobals()->FindObject("DriftTube"));
    const auto WCELL = static_cast<double>(DriftTubeDet->GetConfParF("DriftTube/cellWidth")); 
+
+   //extract alignment parameter from the geometry
+   Float_t dx =0., dy = 0.;
+   TString orientation;
+   if (plane==0)
+   {
+     orientation = "Y";
+     dy = DriftTubeDet->GetConfParF("DriftTube/YdyL"+layer_str);
+   }
+   else
+   {
+     orientation = "X";
+     dx = DriftTubeDet->GetConfParF("DriftTube/XdxL"+layer_str);
+   }
+   Float_t RotX = DriftTubeDet->GetConfParF("DriftTube/"+orientation+"RotX");
+   Float_t RotY = DriftTubeDet->GetConfParF("DriftTube/"+orientation+"RotY");
+   Float_t RotZ = DriftTubeDet->GetConfParF("DriftTube/"+orientation+"RotZL"+layer_str);
 
    if (gGeoManager->cd(node.c_str())) {
       TGeoShape *shape = gGeoManager->GetCurrentNode()->GetVolume()->GetShape();
@@ -83,10 +103,16 @@ TVector3 DriftTubeHit::GetPosition() {
 
          navigator->cd(node.c_str());
          Double_t localPosition[3] {origin[0] + std::min(static_cast<double>((timestamp - drifttube::tped) * drifttube::vdrift), WCELL * 0.5) * laterality, origin[1], origin[2]};
-         Double_t globalPosition[3] {};
-         navigator->LocalToMaster(localPosition, globalPosition);
+         Double_t loc[3] = {0,0,0};
+         localPosition[0] -= (dx+dy); // dx or dy is zero for the respective non-measured coordinate
+         loc[0] = localPosition[0];
+         loc[1] = localPosition[1] + RotY*localPosition[0];
+         loc[2] = localPosition[2] - (RotZ + RotX)*localPosition[0];
 
-         position.SetXYZ(globalPosition[0], globalPosition[1], globalPosition[2]);
+         Double_t globalPosition[3] {};
+         navigator->LocalToMaster(loc, globalPosition);
+
+        position.SetXYZ(globalPosition[0], globalPosition[1], globalPosition[2]);
       }
    }
 
