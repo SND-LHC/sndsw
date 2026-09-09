71eda1636695db13a691fbee7c008e33b1279e78
siilieva <simona.ilieva.ilieva@cern.ch>
2026-09-09 15:47:11 +0200

    dt alignment attempt 3

--- geometry/sndLHC_TI18geom_config.py
+++ geometry/sndLHC_TI18geom_config.py
@@ -401,9 +401,9 @@ with ConfigRegistry.register_config("basic") as c:
           c.DriftTube.DT1LocX,  c.DriftTube.DT1LocY,   c.DriftTube.DT1LocZ    = -451.9*u.mm, -54.25*u.mm, -20.8*u.mm
           c.DriftTube.DT2LocX,  c.DriftTube.DT2LocY,   c.DriftTube.DT2LocZ    = 38.0*u.mm, 10.775*u.mm,  -394.4*u.mm
           # first alignment - tests
-          c.DriftTube.XdxL0, c.DriftTube.XdxL1, c.DriftTube.XdxL2, c.DriftTube.XdxL3 = -0.515*u.cm, -0.532*u.cm, -0.530*u.cm, -0.532*u.cm
-          c.DriftTube.XRotX, c.DriftTube.XRotY = 0.00315*u.rad, 0.0*u.rad
-          c.DriftTube.XRotZL0, c.DriftTube.XRotZL1, c.DriftTube.XRotZL2, c.DriftTube.XRotZL3 = -0.602*u.rad, -0.640*u.rad, -0.637*u.rad, -0.580*u.rad
-          c.DriftTube.YdyL0, c.DriftTube.YdyL1, c.DriftTube.YdyL2, c.DriftTube.YdyL3 = -0.651*u.cm, -0.667*u.cm, -0.664*u.cm, -0.681*u.cm
-          c.DriftTube.YRotX, c.DriftTube.YRotY =  0.0*u.rad, 0.00612*u.rad
-          c.DriftTube.YRotZL0, c.DriftTube.YRotZL1, c.DriftTube.YRotZL2, c.DriftTube.YRotZL3 = -0.038*u.rad, -0.051*u.rad, -0.089*u.rad, -0.009*u.rad
+          c.DriftTube.XdxL0, c.DriftTube.XdxL1, c.DriftTube.XdxL2, c.DriftTube.XdxL3 = -0.26*u.cm, -0.278*u.cm, -0.275*u.cm, -0.276*u.cm
+          c.DriftTube.XRotX, c.DriftTube.XRotY = 0.00327*u.rad, 0.0*u.rad
+          c.DriftTube.XRotZL0, c.DriftTube.XRotZL1, c.DriftTube.XRotZL2, c.DriftTube.XRotZL3 = 7.35*u.mrad, 6.67*u.mrad, 7.33*u.mrad, 6.82*u.mrad
+          c.DriftTube.YdyL0, c.DriftTube.YdyL1, c.DriftTube.YdyL2, c.DriftTube.YdyL3 = -0.114*u.cm, -0.13*u.cm, -0.127*u.cm, -0.144*u.cm
+          c.DriftTube.YRotX, c.DriftTube.YRotY =  0.0*u.rad, 0.00619*u.rad
+          c.DriftTube.YRotZL0, c.DriftTube.YRotZL1, c.DriftTube.YRotZL2, c.DriftTube.YRotZL3 = 6.08*u.mrad, 6.46*u.mrad, 5.9*u.mrad, 6.16*u.mrad
